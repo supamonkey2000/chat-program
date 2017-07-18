3f91cb25e3eca1a584cfea5ad8dee42ff328ad7d
Joshua Moore <joshuamoore235@gmail.com>
2017-07-18 14:09:36 -0600

    Added command line options

--- DesktopApp/chat/src/chat/Start.java
+++ DesktopApp/chat/src/chat/Start.java
@@ -40,12 +40,68 @@ public class Start extends JFrame {
 	}
 	
 	public static void main(String[] args) {
-		try{UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());}catch(Exception ex){ex.printStackTrace();}
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new Start().setVisible(true);
+		if(args.length == 0) {
+			try{UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());}catch(Exception ex){ex.printStackTrace();}
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					new Start().setVisible(true);
+				}
+			});
+		}
+		else {
+			System.out.println("Starting in Headless mode");
+			determine_args(args);
+		}
+	}
+			/** Parameter format:
+			 *  java -jar chatva-d_v_x.x.x.jar <options>
+			 * 	
+			 * 	Options:
+			 * 	-s			Server mode (open Server.java with other params)
+			 * 	-c			Client mode (open Client.java with other params)
+			 * 	-u=<usrnm>	Username to use (default is CLI-user-x)
+			 * 	-a=<adrs>	Use specific IP address
+			 * 	-p=<port>	Use specific port
+			 */
+	public static void determine_args(String[] args) throws ArrayIndexOutOfBoundsException {
+		int tempport = 1500;
+		String tempip = "127.0.0.1";
+		String tempuser = ("CLI-user-" + Integer.toString((int)(Math.random() * 100)));
+		for(int i = 0; i < args.length; i++) {
+			if(args[i].contains("a=")) {
+				String[] temps = args[i].split("=");
+				tempip = temps[1];
 			}
-		});
+			else if(args[i].contains("p=")) {
+				String[] temps = args[i].split("=");
+				tempport = Integer.parseInt(temps[1]);
+			}
+			else if(args[i].contains("u=")) {
+				String[] temps = args[i].split("=");
+				tempuser = temps[1];
+			}
+			if(args[i].contains("help")) {
+				System.out.println("Usage: java -jar chatva-d_v_x.x.x.jar <-s/-c> [-a=address] [-p=port] [-u=username]");
+				System.out.println("");
+				System.out.println("-s		Start in Server mode");
+				System.out.println("-c		Start in Client mode");
+				System.out.println("-a=address	Specify address to use for Client mode (default=127.0.0.1)");
+				System.out.println("-p=port		Specify port to use for either Server or Client mode (default=1500)");
+				System.out.println("-u=username	Specify username to use in Client mode (default=CLI-user-xx)");
+				
+				System.exit(0);
+			}
+		}
+		for(int i = 0; i < args.length; i++) {
+			if(args[i].equals("-s")) {
+				Server server = new Server(tempport);;
+				server.start();
+			}
+			else if(args[i].equals("-c")) {
+				Client client = new Client(tempip, tempport, tempuser);
+				client.start();
+			}
+		}
 	}
 }
